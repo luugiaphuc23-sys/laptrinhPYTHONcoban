275053c306dc12d50ccb25bf22391b18895d3726
Gia Phuc <luugiaphuc23@gmail.com>
2025-11-17 15:50:30 +0700

    Bai2 17/11

---
+++ buoi3/vd1,2,3,4.py
@@ -0,0 +1,22 @@
+i = 2
+while i <= 10:
+    print(f"{i}",end=" ")
+    i += 2
+
+i = 3
+while i <= 18:
+    print(f"{i}",end=" ")
+    i += 3
+
+i = 5
+while i <= 100:
+    print(f"{i}",end=" ")
+    i += 5
+
+i = 1
+while i <= 100:
+    if i!=100:
+        print(f"{i} +",end=" ")
+    else:
+        print(f"100")
+    i += 1
